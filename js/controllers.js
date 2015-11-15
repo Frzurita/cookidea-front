30c25d98c421ecb1d8a082cb1ecc47695aaae41f
Francisco Zurita <zurit92@gmail.com>
2015-11-15 13:05:42 +0100

    first commit

--- js/controllers.js
+++ js/controllers.js
@@ -5,7 +5,7 @@ var homeCtrl = function($scope, $http, $location,$rootScope, $window){
     $scope.ingredientsChosen = [];
     $scope.meal = "";
     
-    $http.post($rootScope.url + 'ingredients/', {
+    $http.post('http://cookidea.co/backend/web/api/ingredients/', {
          email: $window.sessionStorage.getItem('userEmail'),
          password: $window.sessionStorage.getItem('userPassword')
     })
